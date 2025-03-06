b9563832af80762b5335bfb7175f04f6810be26d
Antonio Colella <antoniocolella80@gmail.com>
2025-03-06 23:00:52 +0100

    Aggiornato file

---
+++ esercizi-react/src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// Assuming App.jsx exports the HelloWorld component as App
+ReactDOM.render(<App />, document.getElementById("root"));
