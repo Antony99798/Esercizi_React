00dd65c801914649461ea71540d90aa44fa3f31c
Antonio Colella <antoniocolella80@gmail.com>
2025-03-11 23:26:38 +0100

    aggiunto file

--- esercizi-react/src/Welcome.jsx
+++ esercizi-react/src/Welcome.jsx
@@ -4,7 +4,11 @@ export default function Welcome({name, age}){
     return (
       <div>
         <p>Welcome, {name}!</p>
-        <Age age={age}></Age>
+        {age > 18 ? <Age /> : null}
+        {age && <Age />}
+        {age > 18 && age < 65 ? <Age /> : null}
+        {age > 18 && name === "John" ? <Age /> : null}
+        <Messaggio age={age} />
       </div>
     );
 }
