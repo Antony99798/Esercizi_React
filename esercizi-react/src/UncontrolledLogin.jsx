642c13f515d8789d12095b76ee4157b43301c164
Antonio Colella <antoniocolella80@gmail.com>
2025-03-18 22:46:18 +0100

    Aggiunto file

---
+++ esercizi-react/src/UncontrolledLogin.jsx
@@ -0,0 +1,38 @@
+export default function UncontrolledLogin() {
+
+    function onLogin(event){
+        event.preventDefault()
+
+        const username = event.target.elements.namedItem("username").value;
+        const password = event.target.elements.namedItem("password").value;
+        const remember = event.target.elements.namedItem("remember").checked;
+
+        const data = {
+            username,
+            password,
+            remember,
+        }
+        console.log(data)
+    }
+    function loginWithFormData(event){
+        event.preventDefault();
+
+        const formData = new FormData(event.target)
+
+        const data = {
+          username: formData.get("username"),
+          password: formData.get("password"),
+          remember: formData.get("remember") === 'on' ? true: false,
+        };
+    }
+
+    return (
+      <form onSubmit={onLogin}>
+        <input name="username" />
+        <input name="password" type="password" />
+        <input name="remember" type="checkbox"/>
+        <button onClick={onLogin}>Login</button>
+        <button onClick={loginWithFormData}>Submit</button>
+      </form>
+    );
+}
--- esercizi-react/src/index.jsx
+++ esercizi-react/src/index.jsx
@@ -1,5 +1,5 @@
 import {createRoot} from "react-dom/client"
-import App from "./Chats";
-import Chat from "./Chats";
+import UncontrolledLogin from "./UncontrolledLogin";
 
-createRoot(document.getElementById("root")).render(<Chat/>);
+
+createRoot(document.getElementById("root")).render(<UncontrolledLogin/>);
